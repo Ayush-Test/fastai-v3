afed62f2b372fa4543b2536fb8198010d65b83f0
Ayush-Test <47936289+Ayush-Test@users.noreply.github.com>
2019-02-24 14:13:42 +0530

    Update server.py
    Changing download link for model

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1sPNWuzmbN2Sx13YqmOPNy-JawSOHI0AF'
 export_file_name = 'export.pkl'
 
 classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
